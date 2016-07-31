b193b37f26d4ddcc6368fa3f97c03560ad637b38
nadal28 <nadal_28@hotmail.es>
2016-07-31 12:32:09 +0200

    Add files via upload

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php
+
+$comando_uptime = exec('uptime | tr -s " "');
+
+//------------CPU-------------
+preg_match('/(?<=up\s)(.*?)(?=,\s\d\suser)/', $comando_uptime, $uptime);
+preg_match('/(?<=age:\s)\d+\.\d{2}/', $comando_uptime, $load);
+
+unset($comando_uptime);
+
+//------------RAM------------
+preg_match_all('/\d+/', shell_exec('free -o | tr -s " "'), $ram);
+
+echo json_encode(array($uptime[0],round($load[0]),round(($ram[0][1]/$ram[0][0])*100,2)));
+
+?>
